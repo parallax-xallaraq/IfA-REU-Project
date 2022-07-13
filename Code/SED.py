ff9731990ce9d26dd423434dc2a8dd5f9857ad78
Thresa Kelly <polaris133pc@gmail.com>
2022-07-13 12:14:43 -1000

    better organize SED code

--- Code/SED.py
+++ Code/SED.py
@@ -5,10 +5,48 @@ import matplotlib as mpl
 import plotting as myP
 import numpy as np
 
+#################### COLORS ####################
+
 # custom colormaps 
 red_cmap = mpl.colors.ListedColormap(['#6A040F','#9D0208','#D00000','#DC2F02','#E85D04','#F48C06','#FAA307','#FFBA08'])
 yel_cmap = mpl.colors.ListedColormap(['#3C8802','#6FB505','#A1D907','#D1F520','#FFF705','#F7E200','#EECA00','#DDB100'])
 blu_cmap = mpl.colors.ListedColormap(['#003E6E','#0461A7','#0699C6','#0FCBD1','#24E3C0','#47F08B','#94F76F','#BEF768'])
+
+# returns the colormap corresponding to the key 
+def GetCmap(key='jet', n_ticks=9) :
+    # red
+    if( key=='red' or 
+        key=='r'
+      ) : 
+        return red_cmap
+    # yellow
+    if( key=='yel' or 
+        key=='yellow' or 
+        key=='y'
+      ) : 
+        return yel_cmap
+    # blue 
+    if( key=='blu' or 
+        key=='blue' or 
+        key=='b'
+      ) : 
+        return blu_cmap
+    # rainbow (default)
+    return plt.cm.get_cmap('jet', n_ticks-1)
+
+# plot colorbar 
+def PlotColorbar(cmap, min, max, n_ticks, label):
+    # get tick marks
+    interval = (max - min) / (n_ticks - 1)
+    mult = np.arange(0,n_ticks)
+    ticks = min+(interval*mult)
+    # setup colorbar 
+    norm = mpl.colors.Normalize(vmin=min, vmax=max)
+    sm = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
+    # plot 
+    plt.colorbar(sm, ticks=ticks, label=label)
+
+#################### WAVELENGTHS ####################
 
 # list wavelengths of the photometry filters (in Angstroms) 
 lam_A = np.array([
@@ -39,9 +77,26 @@ def GetObservedWavelengths_A() :
     PrintShape(lam_A)
     return lam_A
 
+#################### HELPER FUNCTIONS ####################
+
+# unit conversions
+def Convert_A_um(ang) : 
+    return (ang * 1E-4)
+def Convert_A_cm(ang) : 
+    return (ang * 1E-8)
+
 # prints shape of array
 def PrintShape(arr) : 
     print('Array shape:\t', np.shape(arr))
+
+# returns array of ID numbers
+def GetID(data) :
+    # get id numbers
+    ids = np.array(data['ID_COSMOS2015'])
+    PrintShape(ids)
+    return ids
+
+#################### SED PREP ####################
 
 # returns array of all observed photometry and their IDs. Bad values and measurements with fracErr are set to NaN.
 def GetPhotometry(data, fracErr=0.5) :
@@ -118,54 +173,36 @@ def GetPhotometry(data, fracErr=0.5) :
     # return table of observed photometry with bad values set to nan
     return flux_nu_uJy
 
-# returns array of ID numbers
-def GetID(data) :
-    # get id numbers
-    ids = np.array(data['ID_COSMOS2015'])
-    PrintShape(ids)
-    return ids
-
 # Returns observed wavelength array
 def ConvertToRestWavelength(redshifts, lamObs=lam_A) : 
     # get table shape
     row = np.shape(redshifts) [0]
     col = np.shape(lamObs) [0]
-
     # initialize table of rest frame wavelengths
     lamRest = np.zeros( (row,col) )
-
     # get rest wavelength 
     for i,z in enumerate(redshifts) : 
         # lambda_rest = lambda_observed / (1+z)
         lamRest[i] = lamObs / (1+z)
-
     # print info
     PrintShape(lamRest)
-
     # return array of rest wavelengths
     return lamRest
 
 # returns lambda*F_lambda after converting F_nu
 def ConvertToEnergyDensity(lamRest_A, Fnu_uJy) : 
-
     # comvert units from angrstroms to centimeters
-    lamRest_cm = lamRest_A * 1E-8
-
+    lamRest_cm = Convert_A_cm(lamRest_A)
     # c[cm/s]/lambda^2
     c_lamR2 = 2.99792458 * 1E10  / lamRest_cm**2
-
     # convert Jy to cgs units: 1 Jy = 10^-23 erg/s/cm2/Hz, uJy = 10^-6 Jy
     Fnu_ergscm2Hz = Fnu_uJy * 1E-23 * 1E-6
-
     # multiply F_nu by c/lam^2 to get F_lam
     Flam_ergscm3 = np.multiply(Fnu_ergscm2Hz, c_lamR2)
-
     # lam * F_lam [erg/s/cm]
     lamFlam_ergscm2 = np.multiply(Flam_ergscm3, lamRest_cm)
-
     # verify same shape 
     PrintShape(lamFlam_ergscm2)
-
     # return lamFlam energy density
     return lamFlam_ergscm2
 
@@ -173,7 +210,7 @@ def ConvertToEnergyDensity(lamRest_A, Fnu_uJy) :
 def NormalizeSED_1um(lamRest_A, lamFlam_ergscm2) :
 
     # convert angstrom to microns
-    lamRest_um = lamRest_A * 1E-4
+    lamRest_um = Convert_A_um(lamRest_A)
 
     # initialize list
     lamFlam_ergscm2_NORM = []
@@ -199,12 +236,15 @@ def NormalizeSED_1um(lamRest_A, lamFlam_ergscm2) :
 
     # return normalized lam*F_lam
     return lamFlam_ergscm2_NORM
-    
+
+#################### SED PLOTTING ####################
+
 # plot SED curves from x and y arrays 
 def PlotSED(
-        x,                  # x-axis data   lam
-        y,                  # y-axis data:  lamFlam
+        x,                  # x-axis data   lam [A]
+        y,                  # y-axis data:  lamFlam [erg/s/cm2]
         cmap='',            # colormap options: red, yel, blu, (jet otherwise)
+        n_ticks=9,          # number of ticks on colorbar
         showBar=True,       # show the colorbar 
         title='',           # plot title
         save='',            # filename to save
@@ -215,24 +255,12 @@ def PlotSED(
         ymax=10**3          #   "    "
     ) : 
 
-    # constant
-    n_ticks = 9
-
-    # convert angstrom to microns
-    x_um = x * 1E-4
-
-    # get number of sources to plot 
-    n = np.shape(x)[0] # row
+    # prepare x
+    x_um = Convert_A_um(x)
+    n = np.shape(x)[0]
 
     # get colormap
-    if  (cmap=='red' or cmap=='r') : 
-        cmap_use = red_cmap
-    elif(cmap=='yel' or cmap=='yellow' or cmap=='y') : 
-        cmap_use = yel_cmap
-    elif(cmap=='blu' or cmap=='blue' or cmap=='b') : 
-        cmap_use = blu_cmap
-    else :
-        cmap_use =  plt.cm.get_cmap('jet', n_ticks-1)
+    cmap_use = GetCmap(cmap, n_ticks)
 
     # transpose y to get 24um column, then take the log
     z = y.T [14]                    # transpose y to get 24um column
@@ -264,51 +292,74 @@ def PlotSED(
     for i in range(n_ye24) : 
         plt.plot(x_ye24[i],y_ye24[i], color=cmap_use(znorm_log_norm_ye24[i]))
 
+    # setup colorbar 
+    if(showBar) :
+        PlotColorbar(
+            cmap=cmap_use, 
+            min=np.nanmin(z_log), 
+            max=np.nanmax(z_log), 
+            n_ticks=n_ticks, 
+            label='$Normalized \; \lambda F_{\lambda} \; at \; 24 \mu m$'
+        )
+    
     # plot median
     if(median) : 
         x_m, y_m = MedianCurve(x_um, y, xmin=1E-1,xmax=1E+1)
         plt.plot(x_m,y_m,c='k',linewidth=2)
 
-    # plot settings
+    # setup plot 
+    PlotSED_Settings( 
+        n=n, 
+        title=title,    save=save, 
+        xmin=xmin,      xmax=xmax, 
+        ymin=ymin,      ymax=ymax 
+    ) 
+
+    # display 
+    plt.show()
+
+    # end 
+    return   
+
+# plot settings for SED
+def PlotSED_Settings(
+        n=0,                # number of sources for text 
+        title='',           # plot title
+        save='',            # filename to save
+        xmin=10**-2.5,      # plot range 
+        xmax=10**3.5,       #   "    "
+        ymin=10**-3,        #   "    "
+        ymax=10**3          #   "    "
+    ) : 
+
+    # scale
     plt.yscale('log')
     plt.xscale('log')
+    plt.grid()
+    # range
     plt.xlim(xmin, xmax)
     plt.ylim(ymin, ymax)
+    # labels 
     plt.xlabel('$\lambda_{rest} \; [\mu m]$') 
     plt.ylabel('$Normalized \; \lambda F_{\lambda} \; [erg \; s^{-1} \; cm^{-2}]$')
-    plt.grid()
-    myP.addtext_n(n)
-
     # axis
     ax = plt.gca()
     ax.set_aspect('equal')
     ax.set_adjustable('box')
     ax.set_xticks([1E-2,1E-1,1E0,1E1,1E2,1E3])
 
-    # setup colorbar 
-    if(showBar) :
-        # get range
-        min=np.nanmin(z_log)
-        max=np.nanmax(z_log)
-        # get tick marks
-        interval = (max - min) / (n_ticks - 1)
-        mult = np.arange(0,n_ticks)
-        ticks = min+(interval*mult)
-        # setup colorbar 
-        norm = mpl.colors.Normalize(vmin=min, vmax=max)
-        sm = plt.cm.ScalarMappable(norm=norm, cmap=cmap_use)
-        plt.colorbar(sm, ticks=ticks, label='$Normalized \; \lambda F_{\lambda} \; at \; 24 \mu m$')
-
+    # set text
+    if(n>0) : 
+        myP.addtext_n(n)
     # set title
     if(title) : 
         plt.title(title)
-
     # save plot as image 
     if(save) : 
         myP.save(save)
-    
-    # display 
-    plt.show()   
+
+    # end
+    return
 
 # returns x and y arrays for the parameter's median curve
 def MedianCurve(x,y,xmin=1E-1,xmax=1E+2) : 
