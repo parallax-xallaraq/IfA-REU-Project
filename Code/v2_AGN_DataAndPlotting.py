efcb8da83d2661707d14dacb89603d42983cd15b
Thresa Kelly <polaris133pc@gmail.com>
2022-11-05 22:09:32 -0500

    small plot param changes

--- Code/v2_AGN_DataAndPlotting.py
+++ Code/v2_AGN_DataAndPlotting.py
@@ -49,7 +49,7 @@ def SetStyle(BIG=16, MID=12, SML=8) :
     mpl.rcParams['axes.titlesize']  = BIG   # size of title
     mpl.rcParams['axes.titlepad']   = BIG   # space between title and plot 
     mpl.rcParams['axes.labelsize']  = MID   # font size of the x and y labels
-    mpl.rcParams['axes.labelpad']   = MID-2 # space between label and axis
+    mpl.rcParams['axes.labelpad']   = SML   # space between label and axis
     # ticks
     mpl.rcParams['xtick.labelsize'] = SML   # font size of the tick labels
     mpl.rcParams['ytick.labelsize'] = SML   # font size of the tick labels
@@ -72,7 +72,7 @@ def AddText_n(n, pre='n = '):
 def Save(filename) :
     plt.savefig(    filename,
                     bbox_inches ="tight",
-                    pad_inches=0.2,
+                    pad_inches=0.05,
                     facecolor='w',
                     edgecolor='w'
     )
